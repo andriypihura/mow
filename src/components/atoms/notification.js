f494d2965fca149681063576755219770354e95a
Andriy Pihura <andriy.pihura@vertalab.com>
2018-06-29 16:08:27 +0300

    Add linter for dev; add proptypes

--- src/components/atoms/notification.js
+++ src/components/atoms/notification.js
@@ -1,54 +1,60 @@
 import React, { Component } from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faHeart as faHeartSolid, faPlus, faPlusCircle } from '@fortawesome/fontawesome-free-solid'
-import { faHeart as faHeartRegular } from '@fortawesome/fontawesome-free-regular'
+import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartSolid, faPlusCircle } from '@fortawesome/fontawesome-free-solid';
+import { faHeart as faHeartRegular } from '@fortawesome/fontawesome-free-regular';
 import './../../css/notification.css';
+const NOTIFY_TIMEOUT = 5000;
 
 class Notification extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       visible: true,
       notification: this.props.notification
-    }
+    };
   }
 
-  componentWillReceiveProps(nextProps) {
-    if(Object.is(this.props.notification, nextProps.notification))
-      return
-    clearTimeout(this.state.timerId)
+  componentDidUpdate(prevProps) {
+    if(Object.is(prevProps.notification, this.props.notification)) return;
+
+    clearTimeout(this.state.timerId);
+
     this.setState({
-      notification: nextProps.notification,
-      timerId: setTimeout(() => this.setState({ notification: null }), 5000)
-    })
+      notification: this.props.notification,
+      timerId: setTimeout(() => this.setState({ notification: null }), NOTIFY_TIMEOUT)
+    });
   }
 
   render(){
-    if(this.state.notification){
-      let { status, type, value } = this.state.notification
-      let info = {
-        icon: null,
-        message: null
-      }
-      switch (type) {
-        case 'like':
-          info.icon = value ? faHeartSolid : faHeartRegular
-          info.message = value ? 'You have liked the recipe' : 'You have removed your like for the recipe'
-          break;
-        case 'addToMenu':
-          info.icon = faPlusCircle
-          info.message = 'You have added the recipe to menu'
-          break;
-      }
-      return (
-        <div className={`notification ${status}`}>
-          <FontAwesomeIcon icon={info.icon} className='notification--icon'/>
-          <div className='notification--text'>{info.message}</div>
-        </div>
-      );
-    } else {
-      return (<div></div>);
+    if (!this.state.notification) return (<div></div>);
+    const { status, type, value } = this.state.notification;
+    const info = {
+      icon: null,
+      message: null
+    };
+    switch (type) {
+    case 'like':
+      [info.icon, info.message] = value
+        ? [faHeartSolid, 'You have liked the recipe']
+        : [faHeartRegular, 'You have removed your like for the recipe'];
+      break;
+    case 'addToMenu':
+      [info.icon, info.message] = [faPlusCircle, 'You have added the recipe to menu'];
+      break;
     }
+    return (
+      <div className={`notification ${status}`}>
+        <FontAwesomeIcon icon={info.icon} className='notification--icon'/>
+        <div className='notification--text'>{info.message}</div>
+      </div>
+    );
   }
 }
+
+Notification.propTypes = {
+  notification: PropTypes.object
+};
+
 export default Notification;
